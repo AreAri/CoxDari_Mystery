511ddc45d1917605fc7b002ee0e6d877fcf85ee4
Arisai <152889517+AreAri@users.noreply.github.com>
2025-05-26 01:00:09 -0600

    Update llenado_CDAM.sql

--- BD/llenado_CDAM.sql
+++ BD/llenado_CDAM.sql
@@ -1,20 +1,20 @@
-INSERT INTO Capitulos (nombre, descripcion, icono, orden, id_cap_ant) VALUES
-('Introducción', 'Conceptos básicos de estructuras de datos, lo que esconde la mascara de programación', NULL, 1, NULL),
-('Apuntadores', 'Todo lo que debes saber del señor Apuntador y sus aliados', NULL, 2, 1),
-('Listas Enlazadas', 'Listas simples y doblemente enlazadas... que tan complidado sera?', NULL, 3, 2),
+INSERT INTO Capitulos (nombre, descripcion, icono, orden) VALUES
+('Introducción', 'Conceptos básicos de estructuras de datos, lo que esconde la mascara de programación', NULL, 1),
+('Apuntadores', 'Todo lo que debes saber del señor Apuntador y sus aliados', NULL, 2),
+('Listas Enlazadas', 'Listas simples y doblemente enlazadas... que tan complidado sera?', NULL, 3);
 
 
-INSERT INTO Niveles (id_cap, tipo, titulo, puntos_recompensa, orden, id_lv_ant) VALUES
-(1, 'Teoria', 'Qué son las ED?', 10, 1, NULL),
-(1, 'Ejercicio', 'Identificar una ED', 20, 2, 1),
-(1, 'Ejercicio', 'Clasifica las ED', 15, 1,2),
-(2, 'Teoria', 'Señor apuntador y sus aliados los formatos', 20, 1, NULL),
-(2, 'Ejercicio', 'Idetifica los direntes formatos', 15, 2,1),
-(2, 'Ejercicio', 'Creando al señor apuntador', 15, 3,2);
+INSERT INTO Niveles (id_cap, tipo, titulo, puntos_recompensa, orden) VALUES
+(1, 'Teoria', 'Qué son las ED?', 10, 1),
+(1, 'Ejercicio', 'Identificar una ED', 20, 2),
+(1, 'Ejercicio', 'Clasifica las ED', 15, 1),
+(2, 'Teoria', 'Señor apuntador y sus aliados los formatos', 20, 1),
+(2, 'Ejercicio', 'Idetifica los direntes formatos', 15, 2),
+(2, 'Ejercicio', 'Creando al señor apuntador', 15, 3);
 
 INSERT INTO Niveles_Teoria (id_lvT, contenido) VALUES
 (1, 'Definición y clasificación de estructuras...'),
-(4, 'Es algo que apunta a algo o una Variable que apunta a la direccion de otra variables o Una forma de hacer referencia a una variable sin tomarla directamente...'),
+(4, 'Es algo que apunta a algo o una Variable que apunta a la direccion de otra variables o Una forma de hacer referencia a una variable sin tomarla directamente...');
 
 INSERT INTO Niveles_Ejercicios (id_lvE, enunciado, solucion) VALUES
 (2, 'Casifica cuales son las estructuras de datos y cuales no ', 'pilas, colas, listas, arboles, grafos'),
@@ -29,4 +29,3 @@ INSERT INTO Personajes (nombre, descripcion, imagen, id_lvT_unlock) VALUES
 INSERT INTO Items (nombre, descripcion, imagen, max_usos, id_personaje) VALUES
 ('Libro de la sabiduria', 'Aqui se ocultas los mayores secretos dentro de la programacion pero sera revelados solo a los elegidos', null, 2, 1),
 ('El cafe del poder', 'La vieja confiable cuando de energias se habla', null, 4, 2);
-
